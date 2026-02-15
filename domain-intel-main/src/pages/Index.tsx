54195792d8288c942b82d83db079e032fd37dda6
Kundan Patil <ikundanpatil@gmail.com>
2026-02-15 14:30:20 +0530

    Delete domain-intel-main/src directory

--- domain-intel-main/src/pages/Index.tsx
+++
@@ -1,7 +0,0 @@
-import { CyberInvestigationDashboard } from "@/components/CyberInvestigationDashboard";
-
-const Index = () => {
-  return <CyberInvestigationDashboard />;
-};
-
-export default Index;
